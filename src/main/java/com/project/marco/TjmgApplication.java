12ceffe7cedbf9a53071674d0d86b9a9c50306c7
marcoantonio40 <marcoudi40@gmail.com>
2021-04-08 21:58:48 -0300

    tirando comentário

--- src/main/java/com/project/marco/TjmgApplication.java
+++ src/main/java/com/project/marco/TjmgApplication.java
@@ -5,7 +5,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
 public class TjmgApplication {
-//teste
+
 	public static void main(String[] args) {
 		SpringApplication.run(TjmgApplication.class, args);
 	}
