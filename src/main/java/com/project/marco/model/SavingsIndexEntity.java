035742f4812e2d0827c7f89eb2b502bcfeb24355
marcoantonio40 <marcoudi40@gmail.com>
2021-05-18 21:59:22 -0300

    TM-21 get make a query

--- src/main/java/com/project/marco/model/SavingsIndexEntity.java
+++ src/main/java/com/project/marco/model/SavingsIndexEntity.java
@@ -2,12 +2,15 @@ package com.project.marco.model;
 
 import lombok.Data;
 
+import javax.persistence.Column;
 import javax.persistence.EmbeddedId;
 import javax.persistence.Entity;
+import javax.persistence.Table;
+import java.io.Serializable;
 
 @Data
 @Entity
-public class SavingsIndexEntity {
+public class SavingsIndexEntity implements Serializable {
 
     @EmbeddedId
     private SavingsIndexId savingsIndexId;
--- src/main/java/com/project/marco/repository/SavingsIndexRepository.java
+++ src/main/java/com/project/marco/repository/SavingsIndexRepository.java
@@ -3,12 +3,17 @@ package com.project.marco.repository;
 import com.project.marco.model.SavingsIndexEntity;
 import com.project.marco.model.SavingsIndexId;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
 
 @Repository
 public interface SavingsIndexRepository extends JpaRepository<SavingsIndexEntity, SavingsIndexId> {
 
-    @Query(value = "select ano, max(mes) from savings_index_entity where ano in ( selec max(ano) from savings_index_entity)")
-    SavingsIndexEntity getLastIndex();
+    @Query("select s from SavingsIndexEntity s where s.savingsIndexId.ano in (select max(s2.savingsIndexId.ano) from SavingsIndexEntity s2)")
+    public List<SavingsIndexEntity> getLastIndex();
 }
